37af3731f97203c4f883e9d5bd1d92d3b5c65e46
henryPy <hoangmainghi.henry.py@gmail.com>
2020-05-21 16:41:51 +0700

    create camera intergration

--- api/face_recognition_api/webcam/views.py
+++ api/face_recognition_api/webcam/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    template = get_template('')
+    return HttpResponse(template.render({}, request))
 
 # Create your views here.
