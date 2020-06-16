a5e3d2fd5d63654dfe8850d514974027b8251118
henryPy <hoangmainghi.henry.py@gmail.com>
2020-06-16 16:39:13 +0700

    update ui for tabWidget

--- client/main.py
+++ client/main.py
@@ -3,7 +3,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 import MySQLdb as db
-from util import ui_loader, db_connector
+from util import ui_loader, db_connector, message_box, file_reader
+from hashlib import md5
 
 
 ui = ui_loader.load_ui('../resources/main.ui')
@@ -16,13 +17,37 @@ class MainApp(QMainWindow, ui):
         self.setupUi(self)
         self.frame_main.setHidden(True)
         self.frame_login.setHidden(False)
-        
-        self.pushButton_login.clicked.connect(self.login)
-        self.lineEdit_password.returnPressed.connect(self.login)
-        self.session = None
+        self.setWindowTitle("Face Access Control")
         self.handle_ui_login()
+        self.handle_buttons()
+        self.handle_ui()
+        self.session = None
 
     def handle_ui(self):
+        self.label_username_session.setStyleSheet("QLabel{font-family: ubuntu 30; color: blue; font-weight: bold}")
+        self.tabWidget_main.tabBar().setVisible(False)
+        self.tabWidget_building_manage.tabBar().setVisible(False)
+
+    def handle_buttons(self):
+        self.pushButton_login.clicked.connect(self.login)
+        self.pushButton_logout.clicked.connect(self.logout)
+        self.lineEdit_password.returnPressed.connect(self.login)
+
+        # handle button for main manager
+        self.pushButton_buiding_manager.clicked.connect(self.open_tab_building)
+        self.pushButton_apartment_manager.clicked.connect(self.open_tab_apartment)
+        self.pushButton_resident_manager.clicked.connect(self.open_tab_resident)
+        self.pushButton_gest_manager.clicked.connect(self.open_tab_guest)
+        self.pushButton_video_access_control.clicked.connect(self.open_tab_access_control)
+
+        #handle button for bulding manager
+        self.pushButton_block_manage.clicked.connect(self.open_tab_block)
+        self.pushButton_floor_manage.clicked.connect(self.open_tab_floor)
+        self.pushButton_door_manage.clicked.connect(self.open_tab_door)
+        self.pushButton_setting_manage.clicked.connect(self.open_tab_setting)
+
+
+    def handle_frame_ui(self):
         self.frame_main.setHidden(not self.frame_main.isHidden())
         self.frame_login.setHidden(not self.frame_login.isHidden())
     
@@ -39,17 +64,54 @@ class MainApp(QMainWindow, ui):
         username = self.lineEdit_username.text()
         password = self.lineEdit_password.text()
         con = db_connector.connector('localhost', 'henrydb', 'root', 'face_recognition')
-        cursor = con.cursor()
-        cursor.execute("select * from user where username='{}' and password='{}'".format(username, password))
-        for i in range(cursor.rowcount):
-            result = cursor.fetchall()
-            if len(result) == 1:
-                self.session = username
-                break
-        if self.session:
-            self.handle_ui()
+        if con == None:
+            msg = message_box.MyMessageBox(QMessageBox.Critical,"Wrong db or authentication", "You must change setting in .config file").exec()
         else:
-            self.label_error.setText('Wrong username or password')
+            cursor = con.cursor()
+            cursor.execute("select * from user where username='{}' and password='{}'".format(username, md5(password.encode()).hexdigest()))
+            for i in range(cursor.rowcount):
+                result = cursor.fetchall()
+                if len(result) == 1:
+                    self.session = username
+                    break
+            if self.session:
+                self.label_username_session.setText(self.session)
+                self.handle_frame_ui()
+                self.lineEdit_username.setText('')
+                self.lineEdit_password.setText('')
+            else:
+                self.label_error.setText('Wrong username or password')
+    
+    def logout(self):
+        self.session = None
+        self.handle_frame_ui()
+
+    def open_tab_building(self):
+        self.tabWidget_main.setCurrentIndex(0)
+
+    def open_tab_apartment(self):
+        self.tabWidget_main.setCurrentIndex(1)
+
+    def open_tab_resident(self):
+        self.tabWidget_main.setCurrentIndex(2)
+
+    def open_tab_guest(self):
+        self.tabWidget_main.setCurrentIndex(3)
+
+    def open_tab_access_control(self):
+        self.tabWidget_main.setCurrentIndex(4)
+
+    def open_tab_block(self):
+        self.tabWidget_building_manage.setCurrentIndex(0)
+    
+    def open_tab_floor(self):
+        self.tabWidget_building_manage.setCurrentIndex(1)
+
+    def open_tab_door(self):
+        self.tabWidget_building_manage.setCurrentIndex(2)
+
+    def open_tab_setting(self):
+        self.tabWidget_building_manage.setCurrentIndex(3)
         
 
 def main():
--- client/util/db_connector.py
+++ client/util/db_connector.py
@@ -10,3 +10,18 @@ def connector(host, username, password, db_name):
         pass
     return db
 
+# def check_login(username, password):
+#     con = connector('localhost', 'henrydb', 'root', 'face_recognitions')
+#     if con == None:
+#         return QMessageBox(QMessageBox.Icon, "Wrong db or authentication")
+#     cusor = con.cursor()
+#     cusor.execute("select * from user where username='{}' and password='{}'".format(username, password.hexdigest()))
+#     print(cusor._last_executed)
+#     session = None
+#     for i in range(cusor.rowcount):
+#         result = cusor.fetchall()
+#         if len(result) == 1:
+#             session = username
+#         else:
+#             break
+#     return session
