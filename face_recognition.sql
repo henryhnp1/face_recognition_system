a8cd63d7a92bd6ec9bc897102166c360189e0112
henryPy <hoangmainghi.henry.py@gmail.com>
2020-06-17 18:21:41 +0700

    chang type unique for type of floor

--- face_recognition.sql
+++ face_recognition.sql
@@ -9,7 +9,7 @@ create table building(
 );
 create table type_of_floor(
 	id int primary key auto_increment,
-    name nvarchar(50) not null
+    name nvarchar(50) not null unique
 );
 create table floor(
 	id int primary key auto_increment,
