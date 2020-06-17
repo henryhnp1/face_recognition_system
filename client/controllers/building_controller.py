c1b2e9dde056fa180ffc727317452d91c575b0ff
henryPy <hoangmainghi.henry.py@gmail.com>
2020-06-17 18:25:10 +0700

    add controller with target to seprate main to many file

---
+++ client/controllers/building_controller.py
@@ -0,0 +1,20 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+import MySQLdb as db
+from util import ui_loader, db_connector, message_box, file_reader
+
+
+
+# def open_tab_block(self):
+#     self.tabWidget_building_manage.setCurrentIndex(0)
+    
+# def open_tab_floor(self):
+#     self.tabWidget_building_manage.setCurrentIndex(1)
+
+# def open_tab_door(self):
+#     self.tabWidget_building_manage.setCurrentIndex(2)
+
+# def open_tab_setting(self):
+#     self.tabWidget_building_manage.setCurrentIndex(3)
